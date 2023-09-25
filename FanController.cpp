87b55b94d65294b798a9d906f48b209920430b10
Dhananjaya <hs.dhananjaya@gmail.com>
2023-09-25 02:03:27 -0700

    Update FanController.cpp

--- FanController.cpp
+++ FanController.cpp
@@ -15,41 +15,56 @@ FanController::FanController(VoltageSensorInterface &temp_sensor_raw,
                                                                    m_fan_relay_enable{fan_relay_enable},
                                                                    m_fan_output_raw{fan_output_raw}
 {
-   std::cout<<"Constructor called"<<std::endl;
+    std::cout << "Constructor called" << std::endl;
 }
 
-void FanController::init() {
+void FanController::init()
+{
     m_sensor_power_enable.setOutput(true);
     std::this_thread::sleep_for(std::chrono::milliseconds(5000));
 }
 
-void FanController::loop() {
-    if(m_temp_sensor_raw.getVoltage() > 60) {
+void FanController::loop()
+{
+    constexpr int enable_fan_relay_threshold_temp = 60;
+    constexpr int disable_fan_relay_threshold_temp = 50;
+    if (m_temp_sensor_raw.getVoltage() > enable_fan_relay_threshold_temp)
+    {
         // enable fan control relay
         m_fan_relay_enable.setOutput(true);
         m_fan_output_raw.setOutputDuty(computePmwOutput());
     }
-    else if(m_temp_sensor_raw.getVoltage() < 50) {
+    else if (m_temp_sensor_raw.getVoltage() < disable_fan_relay_threshold_temp)
+    {
         // disable fan control relay
         m_fan_relay_enable.setOutput(false);
     }
-    else {
-        // do nothing; never reach here
+    else
+    {
+        // TODO: Clarification What should we do if temperature is between 50-60.
+        // for now do thing. Is this the expected behavior?
+        // ; never reach here
     }
 }
 
-float FanController::computePmwOutput() {
+float FanController::computePmwOutput()
+{
     float curTemp = m_temp_sensor_raw.getVoltage();
 
-    if(curTemp >= 50 && curTemp <= 99) {
-        return (curTemp - 50)/2;
+    if (curTemp >= table[0].first && curTemp <= table[0].second)
+    {
+        return (curTemp - 50) / 2;
     }
-    else if(curTemp >= 100 && curTemp <= 129) {
-        return (curTemp - 98)*2 + 20;
+    else if (curTemp >= table[1].first && curTemp <= table[1].second)
+    {
+        return (curTemp - 98) * 2 + 20;
     }
-    else if(curTemp >= 130 && curTemp <= 140) {
+    else if (curTemp >= table[2].first && curTemp <= table[2].second)
+    {
         return 90.0;
-    } else if(curTemp > 140) {
+    }
+    else if (curTemp > table[2].second)
+    {
         return 100.0;
     }
     // error case
